c3fca6f0a1c2f8a631d7015e4eb43b25b96c03fa
TZ2016 <tianhao.zhang.2016@gmail.com>
2013-09-28 14:49:07 -0700

    added obstacle

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,10 +1,11 @@
 class Obstacle {
 
-	final int size;
-	final int position;
-	final int harm;
-	final int picture; // a space holder for GUI
+	final int damage;
+	final int style; // a space holder for GUI
 	
-	Obstacle (int size, ) {}
+	Obstacle (int d, int s) {
+		style = s;
+		damage = d;
+	}
 	
 }
