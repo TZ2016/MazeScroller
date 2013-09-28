78fa77d9db9bb41df9876ddfe6a1455336896337
TZ2016 <tianhao.zhang.2016@gmail.com>
2013-09-28 14:40:53 -0700

    new obstacle

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,5 +1,10 @@
-public class Obstacle {
+class Obstacle {
 
+	final int size;
+	final int position;
+	final int harm;
+	final int picture; // a space holder for GUI
 	
+	Obstacle (int size, ) {}
 	
 }
