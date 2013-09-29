d848a538813dd708b427f9ffcc8f36c517e10ca0
TZ2016 <tianhao.zhang.2016@gmail.com>
2013-09-28 20:17:11 -0700

    gui

--- src/Driver.java
+++ src/Driver.java
@@ -1,14 +1,25 @@
 import java.util.*;
 
-public class Driver {
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Driver extends BasicGame {
+	
 	static Maze maze;
 	static Scene scene;
 	static Layer buffer;
-	public static void main (String[] args) {
-
+	
+	public Driver(String title) {
+		super(title);
+	}
+	
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
 		Scanner sc = new Scanner(System.in);
 		boolean legalargs = false;
-
 		int dif = 0, w = 0, h = 0;
 		while(!legalargs) {
 			System.out.print("Choose difficulti and maze size: \n");
@@ -22,17 +33,37 @@ public class Driver {
 				legalargs = true;
 		}
 
-		// initializing
-		maze = new Maze(new MazeGen(w, h));
 		Scene.DIMENSION = 5; // depends on user input
-		
+		maze = new Maze(new MazeGen(w, h));
 		scene = maze.getScene(Util.UP);
+		buffer = maze.requestLayer(scene.getTop());
+		
+		
 		System.out.print(maze.debugInfo());
 		System.out.println("current scene: \n" + scene.toString());
-		
-		buffer = maze.requestLayer(scene.getTop());
 		System.out.println("current buffer: \n" + buffer.toString());
+	}
+	@Override
+	public void update(GameContainer container, int delta) throws SlickException {
+//		System.out.println(delta);
+	}
+	
+	public void keyPressed(int key, char code) {
+		System.out.println(key + " " + code);
+	}
+	
+	@Override
+	public void render(GameContainer container, Graphics arg1) throws SlickException {
 		
+	}
+	
+	public static void main (String[] args) throws SlickException {
+
+		AppGameContainer app = new AppGameContainer(new Driver("haha"));
+		app.setDisplayMode(800, 600, false);
+		app.start();
+		
+		Scanner sc = new Scanner(System.in);
 		String command = "";
 		sc = new Scanner(System.in);
 		while (true) {
@@ -75,4 +106,5 @@ public class Driver {
 			}
 		}
 	}
+
 }
