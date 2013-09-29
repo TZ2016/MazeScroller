c8257b1a8b6fd807accdb5a5bbdedf43f0b1167a
TZ2016 <tianhao.zhang.2016@gmail.com>
2013-09-28 19:22:50 -0700

    Update Util.java

--- src/Util.java
+++ src/Util.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 
 public class Util {
 	static final int UP = 0; 
 	static final int DOWN = 2; 
 	static final int LEFT = 1; 
-	static final int RIGHT = 3; 
+	static final int RIGHT = 3;
+
 	static void println(Object o) {
 		System.out.println(o);
 	}
