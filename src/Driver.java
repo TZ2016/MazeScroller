8cc32e73155b4380c865d0422a00e11eec9f9f86
Aidan Li <aidan129@sbcglobal.net>
2013-09-28 19:52:49 -0700

    library, pull

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,11 @@
 import java.util.*;
+import org.newdawn.slick.*;
 
-public class Driver {
+public class Driver extends BasicGame {
+	public Driver(String MazeScroller) {
+		super(MazeScroller);
+	}
+	
 	static Maze maze;
 	static Scene scene;
 	static Layer buffer;
@@ -55,4 +60,21 @@ public class Driver {
 			}
 		}
 	}
+	@Override
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private Image player = new Image("player.jpg");
 }
